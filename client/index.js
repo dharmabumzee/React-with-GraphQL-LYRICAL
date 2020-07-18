4cfd0731476197bb1c1e53d8a0646a8dfe0063ee
lukabelani <luka.belani@gmail.com>
2020-07-18 19:29:00 +0200

    remove bug

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import SongList from './components/SongList';
 
