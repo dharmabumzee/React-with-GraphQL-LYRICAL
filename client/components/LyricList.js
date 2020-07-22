e3dc247405976a7229a4aa79f07d8fa865017113
lukabelani <luka.belani@gmail.com>
2020-07-22 18:28:34 +0200

    fetch likes and add optimistic updates

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+// import { optimistic } from 'apollo-client/optimistic-data/store';
 
 class LyricList extends Component {
-    onLike(id) {
-        this.props.mutate({ variables: { id } })
+    onLike(id, likes) {
+        this.props.mutate({ 
+            variables: { id }, 
+            optimisticResponse: {
+                __typename: 'Mutation',
+                likeLyric: {
+                    id,
+                    __typename: 'LyricType',
+                    likes: likes + 1
+                }
+            }
+        })
     }
 
     renderLyrics() {
-        return this.props.lyrics.map(({ id, content }) => {
+        return this.props.lyrics.map(({ id, content, likes }) => {
             return (
                 <li key={id} className="collection-item">
                     {content}
-                    <i className="material-icons" onClick={() => this.onLike(id)}>thumb_up</i>
+                    <div className="vote-box">
+                        <i className="material-icons" onClick={() => this.onLike(id, likes)}>thumb_up</i>
+                        <span className="badge">{likes}</span>
+                    </div>
                 </li>
             );
         })
